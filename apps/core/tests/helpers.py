e4079be1251d2e6245835df557bf9482e0f1f66e
leo <fmleonardo@protonmail.com>
2024-10-22 16:00:35 -0300

    feat: create helper function file
    taken from [dockgrid](https://github.com/dockgrid/dockgrid/blob/main/core/tests/helpers.py)

---
+++ apps/core/tests/helpers.py
@@ -0,0 +1,24 @@
+from django.db.models import QuerySet
+from django.forms import Form
+
+
+class EasyResponse(object):
+    def __init__(self, response):
+        self.response = response
+
+    @property
+    def object_list(self) -> QuerySet:
+        return self.response.context["object_list"]
+
+    @property
+    def account_from_object_list(self):
+        return list(
+            set(self.response.context["object_list"].values_list("account", flat=True))
+        )
+
+    @property
+    def form(self) -> Form:
+        return self.response.context["form"]
+
+
+def add_default_data(): ...
