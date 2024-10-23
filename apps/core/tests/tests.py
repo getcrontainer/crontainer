c5fa727837a5403fcc728b48f216f46f89e90d56
leo <fmleonardo@protonmail.com>
2024-10-23 16:38:55 -0300

    test: create tests for Generic Git credential creation

--- apps/core/tests/tests.py
+++ apps/core/tests/tests.py
@@ -110,3 +110,39 @@ class TestCredentialCreateView(TestCase):
         self.assertEqual(credential_object.category, 4)
         self.assertEqual(credential_object.username, "")
         self.assertTrue(credential_object.id)
+
+    def test_create_generic_git_credentials_failure(self):
+        response = self.client.post(reverse("credential-create"), data={"category": 98})
+        view = EasyResponse(response)
+
+        self.assertFalse(view.form.is_valid())
+
+        self.assertTrue(view.form.errors.get("name"))
+        self.assertFalse(view.form.errors.get("username"))
+        self.assertTrue(view.form.errors.get("password"))
+        self.assertFalse(view.form.errors.get("category"))
+
+        self.assertEqual(view.form.data.get("name"), None)
+        self.assertEqual(view.form.data.get("username"), None)
+        self.assertEqual(view.form.data.get("password"), None)
+        self.assertEqual(view.form.data.get("category"), "98")
+
+    def test_create_generic_git_credentials_success(self):
+        response = self.client.post(
+            reverse("credential-create"),
+            data={
+                "name": "git",
+                "password": "pass",
+                "category": 98,
+            },
+            follow=True,
+        )
+        view = EasyResponse(response)
+
+        credential_object: Credential = view.object_list.first()
+
+        self.assertEqual(credential_object.name, "git")
+        self.assertEqual(credential_object.password, "pass")
+        self.assertEqual(credential_object.category, 98)
+        self.assertEqual(credential_object.username, "")
+        self.assertTrue(credential_object.id)
