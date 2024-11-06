bbcf3f2e6a7c9d8d9edeac3b04b090ff1c4a76dd
leo <fmleonardo@protonmail.com>
2024-11-06 14:49:03 -0300

    test: implement ScheduleCreateView tests

---
+++ apps/core/tests/schedule.py
@@ -0,0 +1,74 @@
+from django.test import TestCase
+
+from apps.core.tests.helpers import EasyResponse
+
+
+class TestScheduleCreateView(TestCase):
+    def test_get(self):
+        response = self.client.get("/create/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "core/schedule_form.html")
+
+    def test_post_empty(self):
+        response = self.client.post(
+            "/create/",
+            {
+                "name": "",
+                "cron_rule": "",
+                "image": "",
+                "credential": "",
+                "cmd": "",
+                "cpu": "",
+                "memory": "",
+                "active": "",
+            },
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "core/schedule_form.html")
+
+        view = EasyResponse(response)
+
+        self.assertEqual(
+            view.form.errors,
+            {
+                "name": ["This field is required."],
+                "cron_rule": ["This field is required."],
+                "image": ["This field is required."],
+            },
+        )
+
+    def test_post_invalid_cron_rule(self):
+        response = self.client.post(
+            "/create/",
+            {
+                "name": "test",
+                "cron_rule": "invalid",
+                "image": "test",
+            },
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "core/schedule_form.html")
+
+        view = EasyResponse(response)
+
+        self.assertEqual(
+            view.form.errors,
+            {
+                "cron_rule": ["Cronjob expression is composed of 5 elements"],
+            },
+        )
+
+    def test_post_valid(self):
+        response = self.client.post(
+            "/create/",
+            {
+                "name": "test",
+                "cron_rule": "0 0 * * *",
+                "image": "test",
+            },
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, "/")
