bb302ed679ddfb657e7c4f45f937bd77fad545cb
leo <fmleonardo@protonmail.com>
2024-11-06 11:35:18 -0300

    refactor: rename tests.py to credential.py
    since it only holds credential specific tests, this should help separate tests for different models

---
+++ apps/core/tests/__init__.py
@@ -0,0 +1,11 @@
+from .credential import (
+    TestCredentialCreateView,
+    TestCredentialDeleteView,
+    TestCredentialListView,
+)
+
+__all__ = [
+    "TestCredentialCreateView",
+    "TestCredentialDeleteView",
+    "TestCredentialListView",
+]
