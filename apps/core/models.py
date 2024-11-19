f7c22276d36de33e9bfabd67c396cb578e347fae
leo <fmleonardo@protonmail.com>
2024-11-19 15:28:05 -0300

    chore: run black

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,7 +1,8 @@
 import uuid
 
 import dateutil
-from cron_descriptor import ExpressionDescriptor, FormatException, MissingFieldException, Options as CronOptions
+from cron_descriptor import ExpressionDescriptor, FormatException, MissingFieldException
+from cron_descriptor import Options as CronOptions
 from django.db import models
 from django.db.models import PROTECT
 from django.utils import timezone
@@ -123,4 +124,3 @@ class Job(models.Model):
             return (end - start).seconds
         except TypeError:
             return "n/a"
-
