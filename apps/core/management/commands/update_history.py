28f1498a5fefb47bbea33aa780cae2305ac6e4dc
Ricardo Pascal <pascal@voorloopnul.com>
2024-12-29 20:57:20 +0100

    refactor: improve job status processing

--- apps/core/management/commands/update_history.py
+++ apps/core/management/commands/update_history.py
@@ -5,30 +5,36 @@ from apps.core.models import Job
 
 
 class Command(BaseCommand):
-    help = "Closes the specified poll for voting"
+    help = "Update job status"
+
+    @staticmethod
+    def _check_container(job: Job):
+        container_name = str(job.id)
+        client = docker.from_env()
+        try:
+            container = client.containers.get(container_name)
+        except docker.errors.NotFound:
+            container = None
+        return container
 
     def handle(self, *args, **options):
-        client = docker.from_env()
         for job in Job.objects.filter(status_code__isnull=True, provisioning=False):
-            print(f"processing job {job.id} for schedule {job.schedule.name}")
-            container_name = str(job.id)
-
-            try:
-                container = client.containers.get(container_name)
-            except docker.errors.NotFound:
-                print("Can´t find this container", container_name)
-                container = None
-
+            self.stdout.write(self.style.WARNING(f"{job.schedule.id} - {job.id} - processing job"))
+            container = self._check_container(job)
             if container:
                 job.state = container.attrs["State"]
                 job.status = container.status
                 job.save()
 
                 if container.status == "exited":
-                    print("Finished job, removing container")
+                    self.stdout.write(
+                        self.style.SUCCESS(f"{job.schedule.id} - {job.id} - finished job, removing container...")
+                    )
                     job.log = container.logs().decode("utf-8")
                     job.status_code = container.wait()["StatusCode"]
                     job.save()
                     container.remove()
-
-            self.stdout.write(self.style.SUCCESS(f"Successfully started Schedule job {job.id}"))
+                else:
+                    self.stdout.write(self.style.WARNING(f"{job.schedule.id} - {job.id} - still running..."))
+            else:
+                self.stdout.write(self.style.ERROR(f"{job.schedule.id} - {job.id} - can't find container"))
