0525f07b178979021be9867b152d281e7652b932
Ricardo Pascal <pascal@voorloopnul.com>
2025-06-29 20:22:00 +0200

    refactor: add resource count and table hover

--- apps/core/templatetags/describe_cron.py
+++ apps/core/templatetags/describe_cron.py
@@ -1,6 +1,9 @@
 from cron_descriptor import ExpressionDescriptor, FormatException, Options
 from django import template
 
+from apps.core.models import Credential, Job, Schedule
+from apps.node.models import Node
+
 register = template.Library()
 
 cron_options = Options()
@@ -15,3 +18,20 @@ def describe_cron(value):
         return ExpressionDescriptor(value, cron_options).get_description()
     except FormatException:
         return "Invalid cron expression"
+
+
+@register.simple_tag
+def resource_count(resource):
+    if resource == "schedule":
+        return Schedule.objects.all().count()
+
+    if resource == "job":
+        return Job.objects.all().count()
+
+    if resource == "credential":
+        return Credential.objects.all().count()
+
+    if resource == "node":
+        return Node.objects.all().count()
+
+    return 0
